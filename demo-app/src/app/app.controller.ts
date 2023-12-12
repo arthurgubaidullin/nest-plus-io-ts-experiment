560eff4a652639c22d9f7f0cdd740335fe1dd3cb
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-12 21:04:47 +0400

    Use codec & type

--- demo-app/src/app/app.controller.ts
+++ demo-app/src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { Response } from './app.controller.response';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,7 +8,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  getData(): Response {
+    const result = this.appService.getData();
+    return Response.encode(result);
   }
 }
