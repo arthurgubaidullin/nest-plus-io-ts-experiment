6718e4739d3567c0460e376cfabefaaa0a554550
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 00:20:43 +0400

    Add tests

---
+++ todos/contracts/create/src/lib/create-todo.spec.ts
@@ -0,0 +1,23 @@
+import { CreateTodo } from './create-todo';
+import { createTodoArb } from './create-todo-arb';
+import fc from 'fast-check';
+
+describe('create-todo', () => {
+  it('should create CreateTodo', () => {
+    fc.assert(
+      fc.property(createTodoArb, (createTodo) => CreateTodo.is(createTodo))
+    );
+  });
+
+  it('should not create CreateTodo', () => {
+    fc.assert(
+      fc.property(fc.object(), (createTodo) => !CreateTodo.is(createTodo))
+    );
+  });
+
+  it('should not create CreateTodo', () => {
+    fc.assert(
+      fc.property(fc.anything(), (createTodo) => !CreateTodo.is(createTodo))
+    );
+  });
+});
