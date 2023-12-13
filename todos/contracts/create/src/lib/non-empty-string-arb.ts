736b77811aa365ff993a2126e560cbdcf3c46c8e
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 00:24:50 +0400

    Fix arb

--- todos/contracts/create/src/lib/non-empty-string-arb.ts
+++ todos/contracts/create/src/lib/non-empty-string-arb.ts
@@ -1,4 +1,6 @@
 import fc from 'fast-check';
 import { NonEmptyString } from 'io-ts-types';
 
-export const nonEmptyStringArb = fc.string().filter(NonEmptyString.is);
+export const nonEmptyStringArb: fc.Arbitrary<NonEmptyString> = fc.string({
+  minLength: 1,
+}) as fc.Arbitrary<NonEmptyString>;
