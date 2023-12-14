e29ac5cd36f6f8621cfa885158b40a769d635ab7
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 23:39:19 +0400

    Add type

--- todos/contracts/create/src/lib/create-todo.ts
+++ todos/contracts/create/src/lib/create-todo.ts
@@ -3,6 +3,8 @@ import { NonEmptyString } from 'io-ts-types';
 
 export interface CreateTodoCommand extends t.TypeOf<typeof CreateTodoCommand> {}
 
+export type EncodedCreateTodoCommand = t.OutputOf<typeof CreateTodoCommand>;
+
 export const CreateTodoCommand = t.readonly(
   t.strict({
     id: NonEmptyString,
