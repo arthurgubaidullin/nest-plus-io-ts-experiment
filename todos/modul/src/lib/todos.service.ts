c1654a2b12e9b645a5565b61811fa9b510c336f0
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 23:28:46 +0400

    Fix service method

--- todos/modul/src/lib/todos.service.ts
+++ todos/modul/src/lib/todos.service.ts
@@ -1,6 +1,5 @@
-import { CreateTodo } from '@nest-plus-io-ts-experiment/create-todo-contract-in-todos';
+import { createTodo } from '@nest-plus-io-ts-experiment/create-todo-in-todos';
 import { TodoDto } from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
-import * as Todo from '@nest-plus-io-ts-experiment/todo-in-todos';
 import { Injectable } from '@nestjs/common';
 import { TodosRepository } from './todos.repository';
 
@@ -8,10 +7,7 @@ import { TodosRepository } from './todos.repository';
 export class TodosService {
   constructor(private readonly repository: TodosRepository) {}
 
-  async create(data: CreateTodo) {
-    const todo = Todo.create(data);
-    return this.repository.create(todo);
-  }
+  public readonly create = createTodo(this.repository);
 
   async getAll(): Promise<readonly TodoDto[]> {
     return this.repository.getAll();
