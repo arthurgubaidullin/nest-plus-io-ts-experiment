6613b3e60b8bdeda97456a69b80b81a127f74118
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 22:20:41 +0400

    Add controller

---
+++ todos/modul/src/lib/todos.controller.ts
@@ -0,0 +1,20 @@
+import { CodecPipe } from '@nest-plus-io-ts-experiment/io-ts-nest';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTodo } from '@nest-plus-io-ts-experiment/create-todo-contract-in-todos';
+import { TodosService } from './todos.service';
+import { TodoDto } from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly repository: TodosService) {}
+
+  @Post()
+  async create(@Body(new CodecPipe(CreateTodo)) data: CreateTodo) {
+    return this.repository.create(data);
+  }
+
+  @Get()
+  async getAll(): Promise<readonly TodoDto[]> {
+    return this.repository.getAll();
+  }
+}
