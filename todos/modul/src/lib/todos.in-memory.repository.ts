8aab80ce63e47459c32db486d7e53976994448be
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-15 00:10:50 +0400

    Fix type

--- todos/modul/src/lib/todos.in-memory.repository.ts
+++ todos/modul/src/lib/todos.in-memory.repository.ts
@@ -1,8 +1,9 @@
+import { TodosRepository } from '@nest-plus-io-ts-experiment/repository-type-in-todos';
 import { TodoDpo } from '@nest-plus-io-ts-experiment/todo-dpo-in-todos';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodosInMemoryRepository {
+export class TodosInMemoryRepository implements TodosRepository {
   private readonly todos: TodoDpo[] = [];
 
   async create(todo: TodoDpo): Promise<void> {
