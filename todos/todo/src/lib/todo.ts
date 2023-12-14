956f0a0691aba625a9765ad5d45a2c3324f94e0f
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 11:30:39 +0400

    Add type

--- todos/todo/src/lib/todo.ts
+++ todos/todo/src/lib/todo.ts
@@ -1,3 +1,18 @@
-export function todo(): string {
-  return 'todo';
+import { NonEmptyString } from 'io-ts-types';
+
+export interface Todo {
+  readonly id: NonEmptyString;
+  readonly content: NonEmptyString;
 }
+
+export const create = (
+  data: Readonly<{
+    id: NonEmptyString;
+    content: NonEmptyString;
+  }>
+): Todo => {
+  return {
+    id: data.id,
+    content: data.content,
+  };
+};
