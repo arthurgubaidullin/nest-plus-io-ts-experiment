2cf9802771297463f4cf29801f48a762bd3787e3
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 20:44:33 +0400

    Fix naming

--- todos/contracts/create/jest.config.ts
+++ todos/contracts/create/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'todos/contracts/create',
+  displayName: 'todos-contracts-create',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
