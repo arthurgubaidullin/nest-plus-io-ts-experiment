3da2530546c05bd51d53a8c4f1a38074521816df
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 20:38:23 +0400

    Add project

---
+++ todos/todo-dto/src/lib/todos-todo-dto.spec.ts
@@ -0,0 +1,7 @@
+import { todosTodoDto } from './todos-todo-dto';
+
+describe('todosTodoDto', () => {
+  it('should work', () => {
+    expect(todosTodoDto()).toEqual('todos-todo-dto');
+  });
+});
---
+++ todos/todo-dto/src/lib/todos-todo-dto.ts
@@ -0,0 +1,3 @@
+export function todosTodoDto(): string {
+  return 'todos-todo-dto';
+}
