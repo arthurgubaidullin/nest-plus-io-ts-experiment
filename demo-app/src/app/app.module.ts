f3048d3e08f0e20cd05c9fa0c564e4cef8946ca1
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 23:30:16 +0400

    Register module

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { TodosModule } from '@nest-plus-io-ts-experiment/modul-in-todos';
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
