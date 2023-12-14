82b0c3b8d8f5e411d57f59fe2bf12cfb6376a66a
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-14 22:40:36 +0400

    Fix service

--- todos/modul/src/lib/todos.service.ts
+++ todos/modul/src/lib/todos.service.ts
@@ -1,18 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTodo } from '@nest-plus-io-ts-experiment/create-todo-contract-in-todos';
 import * as Todo from '@nest-plus-io-ts-experiment/todo-in-todos';
-import * as TodoDto from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TodosService {
   private readonly todos: Todo.Todo[] = [];
 
-  create(data: CreateTodo): void {
-    const todo = Todo.create(data);
+  create(todo: Todo.Todo): void {
     this.todos.push(todo);
   }
 
-  getAll(): readonly TodoDto.TodoDto[] {
+  getAll(): readonly Todo.Todo[] {
     return this.todos;
   }
 }
