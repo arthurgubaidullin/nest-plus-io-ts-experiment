fa3e24bfb0921fe870d919b02c0f25bd2ec07a16
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 00:38:51 +0400

    Add codec

--- todos/todo-dto/src/lib/todos-todo-dto.ts
+++ todos/todo-dto/src/lib/todos-todo-dto.ts
@@ -1,6 +1,11 @@
+import * as t from 'io-ts';
 import { NonEmptyString } from 'io-ts-types';
 
-export interface TodoDto {
-  readonly id: NonEmptyString;
-  readonly content: NonEmptyString;
-}
+export interface TodoDto extends t.TypeOf<typeof TodoDto> {}
+
+export const TodoDto = t.readonly(
+  t.strict({
+    id: NonEmptyString,
+    content: NonEmptyString,
+  })
+);
