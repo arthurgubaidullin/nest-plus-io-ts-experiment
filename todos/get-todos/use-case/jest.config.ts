0e91672064c2f4dfccec315695528721c2e864fe
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 20:19:55 +0400

    Move project

--- todos/get-todos/use-case/jest.config.ts
+++ todos/get-todos/use-case/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'get-todos-in-todos',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/todos/get-todos',
+  coverageDirectory: '../../../coverage/todos/get-todos/use-case',
 };
