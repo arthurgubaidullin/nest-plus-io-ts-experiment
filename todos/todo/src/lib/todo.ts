df3084049577354b7622a2f95021a2e85a6cbc9b
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 18:25:23 +0400

    Add field

--- todos/todo/src/lib/todo.ts
+++ todos/todo/src/lib/todo.ts
@@ -1,9 +1,15 @@
 import * as O from 'fp-ts/Option';
 import { NonEmptyString } from 'io-ts-types';
 
+enum State {
+  InProgress = 'IN_PROGRESS',
+  Completed = 'COMPLETED',
+}
+
 export interface Todo {
   readonly id: NonEmptyString;
   readonly content: NonEmptyString;
+  readonly state: State;
   readonly createdAt: Date;
   readonly updatedAt: O.Option<Date>;
 }
@@ -17,6 +23,7 @@ export const create = (
 ): Todo => {
   return {
     id: data.id,
+    state: State.InProgress,
     content: data.content,
     createdAt: data.createdAt,
     updatedAt: O.none,
