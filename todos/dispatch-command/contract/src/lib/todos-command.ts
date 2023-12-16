3a4f5387abbd28369fe955eef8a78c6fc998366e
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 22:21:19 +0400

    Rename types & codec

--- todos/dispatch-command/contract/src/lib/todos-command.ts
+++ todos/dispatch-command/contract/src/lib/todos-command.ts
@@ -5,14 +5,14 @@ import {
 import { CreateTodoCommand } from '@nest-plus-io-ts-experiment/create-todo-contract-in-todos';
 import * as t from 'io-ts';
 
-export type TodosCommands = t.TypeOf<typeof TodosCommands>;
+export type TodosCommand = t.TypeOf<typeof TodosCommand>;
 
-export type EncodedTodosCommands = t.OutputOf<typeof TodosCommands>;
+export type EncodedTodosCommand = t.OutputOf<typeof TodosCommand>;
 
 const withTag = <T extends string, C extends t.Mixed>(tag: T, codec: C) =>
   t.readonly(t.strict({ _tag: t.literal(tag), value: codec }));
 
-export const TodosCommands = t.union([
+export const TodosCommand = t.union([
   withTag('CreateTodo', CreateTodoCommand),
   withTag('ChangeTodoContent', ChangeTodoContentCommand),
   withTag('ChangeTodoState', ChangeTodoStateCommand),
--- todos/dispatch-command/use-case/src/lib/dispatch-command.ts
+++ todos/dispatch-command/use-case/src/lib/dispatch-command.ts
@@ -4,21 +4,21 @@ import {
   changeTodoState,
 } from '@nest-plus-io-ts-experiment/change-todo-in-todos';
 import { createTodo } from '@nest-plus-io-ts-experiment/create-todo-in-todos';
-import { TodosCommands } from '@nest-plus-io-ts-experiment/dispatch-command-contract-in-todos';
 import {
   CreateTodo,
   GetTodo,
   UpdateTodo,
 } from '@nest-plus-io-ts-experiment/repository-type-in-todos';
 import { FailedToChangeTodo } from '@nest-plus-io-ts-experiment/todo-in-todos';
-import { absurd, pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
+import { absurd, pipe } from 'fp-ts/function';
+import { TodosCommand } from '@nest-plus-io-ts-experiment/dispatch-command-contract-in-todos';
 
 export const dispatchCommand =
   (repo: GetTodo & CreateTodo & UpdateTodo) =>
   (
-    command: TodosCommands
+    command: TodosCommand
   ): Promise<E.Either<NotFoundTodo | FailedToChangeTodo, void>> => {
     switch (command._tag) {
       case 'CreateTodo':
