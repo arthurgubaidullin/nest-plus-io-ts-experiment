a8f37f732af0bce3ac7aec6c4ea3fa0e41c197fa
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 18:27:35 +0400

    Add field

--- todos/todo-dpo/src/lib/todo-dpo.ts
+++ todos/todo-dpo/src/lib/todo-dpo.ts
@@ -1,9 +1,12 @@
 import { Option } from 'fp-ts/Option';
 import { NonEmptyString } from 'io-ts-types';
 
+type State = 'IN_PROGRESS' | 'COMPLETED';
+
 export interface TodoDpo {
   readonly id: NonEmptyString;
   readonly content: NonEmptyString;
+  readonly state: State;
   readonly createdAt: Date;
   readonly updatedAt: Option<Date>;
 }
