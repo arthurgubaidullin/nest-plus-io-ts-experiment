66bb5db04180daed4ce0bdb7f57fa339f4fe1010
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 17:45:38 +0400

    Fix controller

--- todos/modul/src/lib/todos.controller.ts
+++ todos/modul/src/lib/todos.controller.ts
@@ -1,9 +1,10 @@
 import { CreateTodoCommand } from '@nest-plus-io-ts-experiment/create-todo-contract-in-todos';
 import { CodecPipe } from '@nest-plus-io-ts-experiment/io-ts-nest';
-import { TodoDto } from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
+import {
+  TodoDto,
+  TodoDtoList,
+} from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import * as RA from 'fp-ts/ReadonlyArray';
-import { pipe } from 'fp-ts/function';
 import { OutputOf } from 'io-ts';
 import { TodosService } from './todos.service';
 
@@ -21,6 +22,6 @@ export class TodosController {
   @Get()
   async getAll(): Promise<readonly OutputOf<typeof TodoDto>[]> {
     const todos = await this.service.getList();
-    return pipe(todos, RA.map(TodoDto.encode));
+    return TodoDtoList.encode(todos);
   }
 }
