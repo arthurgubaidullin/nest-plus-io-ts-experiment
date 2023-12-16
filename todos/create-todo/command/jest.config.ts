f62ba8b4668d02103e0e8e892e8f14072cfce8d7
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 20:05:02 +0400

    Move project

--- todos/create-todo/command/jest.config.ts
+++ todos/create-todo/command/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'create-todo-contract-in-todos',
+  displayName: 'create-todo-command-in-todos',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/todos/contracts/create',
+  coverageDirectory: '../../../coverage/todos/create-todo/command',
 };
