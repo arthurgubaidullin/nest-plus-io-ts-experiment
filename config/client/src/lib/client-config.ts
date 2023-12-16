43bb69b4c6020870b2bc480b8de731882869d068
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 00:24:16 +0400

    Add type

--- config/client/src/lib/client-config.ts
+++ config/client/src/lib/client-config.ts
@@ -1,3 +1,19 @@
-export function clientConfig(): string {
-  return 'client-config';
+import * as O from 'fp-ts/Option';
+import { identity, pipe } from 'fp-ts/function';
+import { createContext, useContext } from 'react';
+
+interface Config {
+  readonly host: string;
 }
+
+const ConfigContext = createContext<O.Option<Config>>(O.none);
+
+export const ConfigProvider = ConfigContext.Provider;
+
+export const useClientConfig = (): Config =>
+  pipe(
+    useContext(ConfigContext),
+    O.fold(() => {
+      throw new TypeError();
+    }, identity)
+  );
