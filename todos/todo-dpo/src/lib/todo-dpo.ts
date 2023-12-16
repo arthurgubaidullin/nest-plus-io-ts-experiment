05c3c5d8fb64911e413854dca6668bb2a6a4a0f3
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 18:04:59 +0400

    Add field

--- todos/todo-dpo/src/lib/todo-dpo.ts
+++ todos/todo-dpo/src/lib/todo-dpo.ts
@@ -1,7 +1,9 @@
+import { Option } from 'fp-ts/Option';
 import { NonEmptyString } from 'io-ts-types';
 
 export interface TodoDpo {
   readonly id: NonEmptyString;
   readonly content: NonEmptyString;
   readonly createdAt: Date;
+  readonly updatedAt: Option<Date>;
 }
