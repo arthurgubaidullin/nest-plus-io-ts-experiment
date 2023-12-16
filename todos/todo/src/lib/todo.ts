cc262821c8fc1abcaa1d39331aaf3b6e2695c97a
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 17:54:20 +0400

    Add field

--- todos/todo/src/lib/todo.ts
+++ todos/todo/src/lib/todo.ts
@@ -3,16 +3,19 @@ import { NonEmptyString } from 'io-ts-types';
 export interface Todo {
   readonly id: NonEmptyString;
   readonly content: NonEmptyString;
+  readonly createdAt: Date;
 }
 
 export const create = (
   data: Readonly<{
     id: NonEmptyString;
     content: NonEmptyString;
+    createdAt: Date;
   }>
 ): Todo => {
   return {
     id: data.id,
     content: data.content,
+    createdAt: data.createdAt,
   };
 };
