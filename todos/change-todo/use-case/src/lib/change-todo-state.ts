92462f3c9ec74c57fe11428323e3e0d58ece5dd3
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-16 21:49:22 +0400

    Add function

---
+++ todos/change-todo/use-case/src/lib/change-todo-state.ts
@@ -0,0 +1,24 @@
+import { ChangeTodoStateCommand } from '@nest-plus-io-ts-experiment/change-todo-contract-in-todos';
+import {
+  GetTodo,
+  UpdateTodo,
+} from '@nest-plus-io-ts-experiment/repository-type-in-todos';
+import * as Todo from '@nest-plus-io-ts-experiment/todo-in-todos';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+import { NotFoundTodo } from './not-found-todo';
+
+export const changeTodoState =
+  (repository: GetTodo & UpdateTodo) =>
+  async (
+    data: ChangeTodoStateCommand
+  ): Promise<E.Either<NotFoundTodo | Todo.FailedToChangeTodo, void>> =>
+    pipe(
+      async () => await repository.get(data.id),
+      TE.fromTaskOption(() => new NotFoundTodo()),
+      TE.map(Todo.fromDpo),
+      TE.chainEitherKW(Todo.changeState(data)),
+      TE.chainTaskK((todo) => async () => await repository.update(todo)),
+      (t) => t()
+    );
