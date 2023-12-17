7cb0d52f341053d14751a3b0ea2eb832ee3508bb
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 16:59:25 +0400

    Rename type

--- todos/state-manager/type/src/lib/state-manager.ts
+++ todos/state-manager/type/src/lib/state-manager.ts
@@ -13,4 +13,4 @@ export interface ToggleTodo {
   readonly toggleTodo: (todoId: NonEmptyString) => Promise<void>;
 }
 
-export type StateManagerApi = GetTodos & AddTodo & ToggleTodo;
+export type TodosStateManager = GetTodos & AddTodo & ToggleTodo;
