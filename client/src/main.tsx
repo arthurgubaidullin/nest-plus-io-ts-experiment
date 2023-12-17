27c34bf66a94a8e37d22c7d69e74bc28c23dc150
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 17:24:09 +0400

    Fix client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
-import { TodosServerApiProvider } from '@nest-plus-io-ts-experiment/server-api-context-in-todos';
 import { getServerApi } from '@nest-plus-io-ts-experiment/server-api-in-todos';
+import { TodosStateManagerProvider } from '@nest-plus-io-ts-experiment/state-manager-context-in-todos';
+import { getTodosStateManager } from '@nest-plus-io-ts-experiment/state-manager-in-todos';
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,14 +12,17 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const todosStateManager = pipe(
+  getServerApi({ host: '//localhost:3000' }),
+  getTodosStateManager
+);
+
 root.render(
   <StrictMode>
-    <TodosServerApiProvider
-      value={O.some(getServerApi({ host: '//localhost:3000' }))}
-    >
+    <TodosStateManagerProvider value={O.some(todosStateManager)}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </TodosServerApiProvider>
+    </TodosStateManagerProvider>
   </StrictMode>
 );
