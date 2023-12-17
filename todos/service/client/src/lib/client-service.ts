83e06adaa2c6ef8fe51caa644a93dd0df0b1cd3f
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 13:48:13 +0400

    Fix style

--- todos/service/client/src/lib/client-service.ts
+++ todos/service/client/src/lib/client-service.ts
@@ -2,9 +2,11 @@ import { dispatchCommand } from '@nest-plus-io-ts-experiment/dispatch-command-vi
 import { getTodos } from '@nest-plus-io-ts-experiment/get-todos-via-fetch-in-todos';
 import { TodosService } from '@nest-plus-io-ts-experiment/service-type-in-todos';
 
-export const getService = (data: Readonly<{ host: string }>): TodosService => {
+export const getService = (
+  config: Readonly<{ host: string }>
+): TodosService => {
   return {
-    dispatch: dispatchCommand(data),
-    getList: () => getTodos(data),
+    dispatch: dispatchCommand(config),
+    getList: () => getTodos(config),
   };
 };
