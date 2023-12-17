f54437fc02bc4922d2ed1510416f937618cd40fa
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 14:09:52 +0400

    Fix style

--- todos/service/context/src/lib/client-service-context.ts
+++ todos/service/context/src/lib/client-service-context.ts
@@ -1,9 +1,9 @@
+import { TodosService } from '@nest-plus-io-ts-experiment/service-type-in-todos';
 import * as O from 'fp-ts/Option';
-import { createContext, useContext } from 'react';
-import { TodosServiceConfig } from '@nest-plus-io-ts-experiment/client-service-config-in-todos';
 import { identity, pipe } from 'fp-ts/function';
+import { createContext, useContext } from 'react';
 
-const TodosServiceContext = createContext<O.Option<TodosServiceConfig>>(O.none);
+const TodosServiceContext = createContext<O.Option<TodosService>>(O.none);
 
 export const TodosServiceProvider = TodosServiceContext.Provider;
 
