c7628847ab63b4187a4701d0e19f7080ad51392e
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 14:07:26 +0400

    Add things

--- todos/service/context/src/lib/client-service-context.ts
+++ todos/service/context/src/lib/client-service-context.ts
@@ -1,3 +1,16 @@
-export function clientServiceContextInTodos(): string {
-  return 'client-service-context-in-todos';
-}
+import * as O from 'fp-ts/Option';
+import { createContext, useContext } from 'react';
+import { TodosServiceConfig } from '@nest-plus-io-ts-experiment/client-service-config-in-todos';
+import { identity, pipe } from 'fp-ts/function';
+
+const TodosServiceContext = createContext<O.Option<TodosServiceConfig>>(O.none);
+
+export const TodosServiceProvider = TodosServiceContext.Provider;
+
+export const useTodosServiceContext = () =>
+  pipe(
+    useContext(TodosServiceContext),
+    O.fold(() => {
+      throw new TypeError('Not found TodosServiceContext.');
+    }, identity)
+  );
