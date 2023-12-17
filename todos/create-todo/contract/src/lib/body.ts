e20527efe7949d70f359c333cf10f1a57528c556
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 19:24:28 +0400

    Remove unused

--- todos/create-todo/contract/src/lib/body.ts
+++
@@ -1,8 +0,0 @@
-import { CreateTodoCommand } from '@nest-plus-io-ts-experiment/create-todo-command-in-todos';
-import * as t from 'io-ts';
-
-export interface Body extends t.TypeOf<typeof Body> {}
-
-export type EncodedBody = t.OutputOf<typeof Body>;
-
-export const Body = CreateTodoCommand;
