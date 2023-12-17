811f7003d2e529c5d95d17ee2a6b059d7cfc2d71
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 19:45:15 +0400

    Add function

--- todos/data-objects/response/src/lib/response.ts
+++ todos/data-objects/response/src/lib/response.ts
@@ -1,3 +1,17 @@
-export function responseInTodos(): string {
-  return 'response-in-todos';
-}
+import * as t from 'io-ts';
+
+export const getResponseCodec = <C extends t.Mixed>(codec: C) =>
+  t.union([
+    t.readonly(
+      t.strict({
+        ok: t.literal(true),
+        data: codec,
+      })
+    ),
+    t.readonly(
+      t.strict({
+        ok: t.literal(false),
+        errors: t.union([t.string, t.readonlyArray(t.string)]),
+      })
+    ),
+  ]);
