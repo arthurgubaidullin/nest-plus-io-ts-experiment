aadd7a81fb106d8abfd9d3adc38ab042b3675874
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 17:30:35 +0400

    Fix type

--- todos/state-manager/type/src/lib/state-manager.ts
+++ todos/state-manager/type/src/lib/state-manager.ts
@@ -1,5 +1,12 @@
+import { RemoteData } from '@devexperts/remote-data-ts';
+import { GetTodosResponse } from '@nest-plus-io-ts-experiment/get-todos-contract-in-todos';
+import { FailedToGetTodos } from '@nest-plus-io-ts-experiment/server-api-type-in-todos';
 import { TodoDto } from '@nest-plus-io-ts-experiment/todo-dto-in-todos';
 import { NonEmptyString } from 'io-ts-types';
+
+export interface UseTodos {
+  readonly useTodos: () => RemoteData<FailedToGetTodos, GetTodosResponse>;
+}
 
 export interface GetTodos {
   readonly getTodos: () => Promise<void>;
@@ -13,4 +20,4 @@ export interface ToggleTodo {
   readonly toggleTodo: (todoId: NonEmptyString) => Promise<void>;
 }
 
-export type TodosStateManager = GetTodos;
+export type TodosStateManager = UseTodos & GetTodos;
