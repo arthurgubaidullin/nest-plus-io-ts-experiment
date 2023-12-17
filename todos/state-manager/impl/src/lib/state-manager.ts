e95e8538e1c972b59e41dda1d32ab4f0c4183dcc
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 17:55:56 +0400

    Fix function

--- todos/state-manager/impl/src/lib/state-manager.ts
+++ todos/state-manager/impl/src/lib/state-manager.ts
@@ -1,9 +1,10 @@
+import { isSuccess } from '@devexperts/remote-data-ts';
 import { TodosServerApi } from '@nest-plus-io-ts-experiment/server-api-type-in-todos';
 import { TodosStateManager } from '@nest-plus-io-ts-experiment/state-manager-type-in-todos';
 import { useTodosStore } from '@nest-plus-io-ts-experiment/todos-store';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
+import { constVoid, pipe } from 'fp-ts/function';
 
 export const getTodosStateManager = (
   api: TodosServerApi
@@ -30,7 +31,11 @@ export const getTodosStateManager = (
         (t) => t()
       ),
     getTodos: pipe(
-      TE.fromIO(() => useTodosStore.getState().pending()),
+      TE.fromIO(() =>
+        !isSuccess(useTodosStore.getState().todos)
+          ? useTodosStore.getState().pending()
+          : constVoid()
+      ),
       TE.chain(() => api.getList()),
       TE.fold(
         (e) =>
