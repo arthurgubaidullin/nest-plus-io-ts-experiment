c994aaa6486dec9fae968ab740ce1bc9b0bd9301
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 13:28:51 +0400

    Add function

--- todos/service/client/src/lib/client-service.ts
+++ todos/service/client/src/lib/client-service.ts
@@ -1,3 +1,11 @@
-export function clientServiceInTodos(): string {
-  return 'client-service-in-todos';
-}
+import { getTodos } from '@nest-plus-io-ts-experiment/get-todos-via-fetch-in-todos';
+import { TodosService } from '@nest-plus-io-ts-experiment/service-type-in-todos';
+
+export const getService = (data: Readonly<{ host: string }>): TodosService => {
+  return {
+    dispatch: () => async () => {
+      throw new Error('Unimplemented.');
+    },
+    getList: () => getTodos(data),
+  };
+};
