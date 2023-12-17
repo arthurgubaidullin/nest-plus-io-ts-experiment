0bb3346581e022fb277b6e975caa7aafe605584d
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 19:57:58 +0400

    Fix function

--- todos/dispatch-command/via-fetch/src/lib/dispatch-command.ts
+++ todos/dispatch-command/via-fetch/src/lib/dispatch-command.ts
@@ -1,9 +1,14 @@
-import { DispatchCommandBody } from '@nest-plus-io-ts-experiment/dispatch-command-contract-in-todos';
+import {
+  DispatchCommandBody,
+  DispatchCommandResponse,
+} from '@nest-plus-io-ts-experiment/dispatch-command-contract-in-todos';
 import { FailedToDispatchCommand } from '@nest-plus-io-ts-experiment/server-api-type-in-todos';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
+import * as T from 'fp-ts/Task';
 import { TaskEither } from 'fp-ts/TaskEither';
 import { constVoid, pipe } from 'fp-ts/function';
+import { failure } from 'io-ts/PathReporter';
 
 export const dispatchCommand =
   (config: Readonly<{ host: string }>) =>
@@ -17,12 +22,16 @@ export const dispatchCommand =
           },
           body: JSON.stringify(DispatchCommandBody.encode(data)),
         }),
+      T.chain((resp) => async (): Promise<unknown> => resp.json()),
       (f) => TE.tryCatch(f, E.toError),
+      TE.chainEitherKW((data) =>
+        pipe(data, DispatchCommandResponse.decode, E.mapLeft(failure))
+      ),
       TE.tapError((e) =>
         TE.fromIO(() => {
           console.error(e);
         })
       ),
-      TE.mapLeft((e) => new FailedToDispatchCommand(e)),
+      TE.mapLeft((e) => new FailedToDispatchCommand(E.toError(e))),
       TE.map(constVoid)
     );
