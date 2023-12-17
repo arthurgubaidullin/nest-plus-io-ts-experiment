c4fd9f8f01e319b82b2611ceb51809e4321ff9e8
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 14:01:02 +0400

    Refactor

--- todos/service/client/src/lib/client-service.ts
+++ todos/service/client/src/lib/client-service.ts
@@ -1,10 +1,9 @@
+import { TodosServiceConfig } from '@nest-plus-io-ts-experiment/client-service-config-in-todos';
 import { dispatchCommand } from '@nest-plus-io-ts-experiment/dispatch-command-via-fetch-in-todos';
 import { getTodos } from '@nest-plus-io-ts-experiment/get-todos-via-fetch-in-todos';
 import { TodosService } from '@nest-plus-io-ts-experiment/service-type-in-todos';
 
-export const getService = (
-  config: Readonly<{ host: string }>
-): TodosService => {
+export const getService = (config: TodosServiceConfig): TodosService => {
   return {
     dispatch: dispatchCommand(config),
     getList: () => getTodos(config),
