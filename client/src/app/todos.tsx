96510751bcd84678ed1b75fc4029b49f6f2ed29f
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 14:53:06 +0400

    Fix component

--- client/src/app/todos.tsx
+++ client/src/app/todos.tsx
@@ -1,6 +1,7 @@
 import { fold3 } from '@devexperts/remote-data-ts';
 import { useTodos } from '@nest-plus-io-ts-experiment/use-todos-in-todos';
 import { pipe } from 'fp-ts/function';
+import { NewTodoForm } from './new-todo-form';
 
 export const TodoList = () => {
   const todos = useTodos();
@@ -12,6 +13,10 @@ export const TodoList = () => {
       (todos) => (
         <div>
           <h1>Todos</h1>
+          <br />
+          <NewTodoForm />
+          <br />
+          <h2>List </h2>
           <ul>
             {todos.map((todo) => (
               <li key={todo.id}>
