e33828795e27e913389e97c21c761c4068a91bc8
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 18:05:40 +0400

    Fix impl

--- todos/state-manager/impl/src/lib/state-manager.ts
+++ todos/state-manager/impl/src/lib/state-manager.ts
@@ -1,7 +1,10 @@
-import { isSuccess } from '@devexperts/remote-data-ts';
+import * as RD from '@devexperts/remote-data-ts';
+import { ChangeTodoStateCommand } from '@nest-plus-io-ts-experiment/change-todo-contract-in-todos';
 import { TodosServerApi } from '@nest-plus-io-ts-experiment/server-api-type-in-todos';
 import { TodosStateManager } from '@nest-plus-io-ts-experiment/state-manager-type-in-todos';
 import { useTodosStore } from '@nest-plus-io-ts-experiment/todos-store';
+import * as O from 'fp-ts/Option';
+import { findFirst } from 'fp-ts/ReadonlyArray';
 import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 import { constVoid, pipe } from 'fp-ts/function';
@@ -10,6 +13,35 @@ export const getTodosStateManager = (
   api: TodosServerApi
 ): TodosStateManager => {
   const manager: TodosStateManager = {
+    toggleTodo: (todoId) =>
+      pipe(
+        useTodosStore.getState().todos,
+        RD.toOption,
+        O.chain(findFirst((a) => a.id === todoId)),
+        TE.fromOption(() => 'Not found todo.' as const),
+        TE.map(
+          (a) =>
+            ({
+              _tag: 'ChangeTodoState' as const,
+              id: todoId,
+              state: a.state,
+              updatedAt: new Date(),
+            } satisfies ChangeTodoStateCommand)
+        ),
+        TE.chainW(api.dispatch),
+        TE.fold(
+          (e) =>
+            T.fromIO(() => {
+              console.error(e);
+            }),
+          () =>
+            T.fromIO(() => {
+              manager.getTodos();
+            })
+        ),
+        (t) => t()
+      ),
+
     useTodos: () => {
       const { todos } = useTodosStore();
       return todos;
@@ -32,7 +64,7 @@ export const getTodosStateManager = (
       ),
     getTodos: pipe(
       TE.fromIO(() =>
-        !isSuccess(useTodosStore.getState().todos)
+        !RD.isSuccess(useTodosStore.getState().todos)
           ? useTodosStore.getState().pending()
           : constVoid()
       ),
