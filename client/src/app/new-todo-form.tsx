e88c117d486db50c5aea50464168469c601fd4fd
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-17 14:50:49 +0400

    Add compnent

---
+++ client/src/app/new-todo-form.tsx
@@ -0,0 +1,27 @@
+import { useTodosServiceContext } from '@nest-plus-io-ts-experiment/client-service-context-in-todos';
+import { NonEmptyString } from 'io-ts-types';
+
+export const NewTodoForm = () => {
+  const service = useTodosServiceContext();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        const fd = new FormData(e.currentTarget);
+
+        service.dispatch({
+          _tag: 'CreateTodo',
+          content: fd.get('content') as NonEmptyString,
+          createdAt: new Date(),
+          id: crypto.randomUUID() as NonEmptyString,
+        });
+      }}
+    >
+      <label>
+        Todo: <input type="text" name="content" required />
+      </label>
+      <input type="submit" />
+    </form>
+  );
+};
